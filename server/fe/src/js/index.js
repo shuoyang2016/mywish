3266a921f9fce80f1d507a636e706a8e45a13d02
Shuo Yang <ys56555@gmail.com>
2018-03-11 21:01:30 -0700

    use webpack

---
+++ server/fe/src/js/index.js
@@ -0,0 +1,26 @@
+import '../css/cover.css';
+import '../css/general.css';
+import $ from 'jquery';
+
+
+$('#recipeCarousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    console.info("This is called.")
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+
+    for (var i=0;i<2;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
--- server/fe/webpack.config.js
+++ server/fe/webpack.config.js
@@ -1,13 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/js/index.js',
+  'jquery'
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  externals: {
+    jquery: 'jQuery'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
   devServer: {
     contentBase: './dist'
-  }
+  },
+  plugins: [
+      new HtmlWebpackPlugin({template: './src/templates/index.html'})
+  ]
 };
 
